82cd65167d8484c9f2b75a0192b5f549cc19dead
ktym4a <shoma@ktym4a.me>
2023-12-29 20:43:16 +0700

    Add useNavigation hook to App component

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,7 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useNavigation,
 } from "@remix-run/react"
 import appStylesHref from "./app.css"
 import { createEmptyContact, getContacts } from "./data"
@@ -34,6 +35,7 @@ export const loader = async () => {
 // biome-ignore lint/nursery/noDefaultExport: <explanation>
 export default function App() {
   const { contacts } = useLoaderData<typeof loader>()
+  const navigation = useNavigation()
 
   return (
     <html lang="en">
@@ -91,7 +93,10 @@ export default function App() {
             )}
           </nav>
         </div>
-        <div id="detail">
+        <div
+          className={navigation.state === "loading" ? "loading" : ""}
+          id="detail"
+        >
           <Outlet />
         </div>
 
